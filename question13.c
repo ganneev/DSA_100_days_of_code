769173c7116f01770cd95428a10f1c95f868f663
ganneev <ganeevkaur2007@gmail.com>
2026-03-06 12:48:21 +0530

    Add files via upload

---
+++ question13.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+    int r, c;
+    printf("Enter the number of rows and columns: ");
+    scanf("%d %d", &r, &c);
+    int a[r][c];
+    for(int i = 0; i < r; i++) {
+        for(int j = 0; j < c; j++) {
+            printf("Enter element a[%d][%d]: ", i, j);
+            scanf("%d", &a[i][j]);
+        }
+    }
+    int top = 0, bottom = r - 1;
+    int left = 0, right = c - 1;
+    while(top <= bottom && left <= right) {
+        for(int i = left; i <= right; i++)
+            printf("%d ", a[top][i]);
+        top++;
+        for(int i = top; i <= bottom; i++)
+            printf("%d ", a[i][right]);
+        right--;
+        if(top <= bottom) {
+            for(int i = right; i >= left; i--)
+                printf("%d ", a[bottom][i]);
+            bottom--;
+        }
+        if(left <= right) {
+            for(int i = bottom; i >= top; i--)
+                printf("%d ", a[i][left]);
+            left++;
+        }
+    }
+    return 0;
+}
