6aca5a2e41482c8b741b38dcbdc28d1d0981d794
ganneev <ganeevkaur2007@gmail.com>
2026-03-08 15:58:50 +0530

    Add files via upload

---
+++ question16.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int a[n];
+    int visited[n];
+
+    for(int i = 0; i < n; i++) {
+        printf("Enter element a[%d]: ", i);
+        scanf("%d", &a[i]);
+        visited[i] = 0;
+    }
+
+    for(int i = 0; i < n; i++) {
+        if(visited[i] == 1)
+            continue;
+
+        int count = 1;
+
+        for(int j = i + 1; j < n; j++) {
+            if(a[i] == a[j]) {
+                count++;
+                visited[j] = 1;
+            }
+        }
+
+        printf("%d:%d ", a[i], count);
+    }
+
+    return 0;
+}
