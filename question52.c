23a1e96dcc0b05de07314861493188c0d1642469
ganneev <ganeevkaur2007@gmail.com>
2026-04-09 10:45:06 +0530

    Add files via upload

---
+++ question52.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+// create node
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+// build tree from level order
+struct Node* buildTree(int arr[], int n) {
+    if(n == 0 || arr[0] == -1) return NULL;
+
+    struct Node* root = createNode(arr[0]);
+
+    struct Node* queue[n];
+    int front = 0, rear = 0;
+
+    queue[rear++] = root;
+    int i = 1;
+
+    while(i < n) {
+        struct Node* curr = queue[front++];
+
+        if(i < n && arr[i] != -1) {
+            curr->left = createNode(arr[i]);
+            queue[rear++] = curr->left;
+        }
+        i++;
+
+        if(i < n && arr[i] != -1) {
+            curr->right = createNode(arr[i]);
+            queue[rear++] = curr->right;
+        }
+        i++;
+    }
+
+    return root;
+}
+
+// find LCA in Binary Tree
+struct Node* LCA(struct Node* root, int n1, int n2) {
+    if(root == NULL)
+        return NULL;
+
+    // if one of the nodes matches
+    if(root->data == n1 || root->data == n2)
+        return root;
+
+    struct Node* left = LCA(root->left, n1, n2);
+    struct Node* right = LCA(root->right, n1, n2);
+
+    // if both sides return non-null → this is LCA
+    if(left && right)
+        return root;
+
+    // otherwise return non-null child
+    return (left != NULL) ? left : right;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for(int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    struct Node* root = buildTree(arr, n);
+
+    int n1, n2;
+    scanf("%d %d", &n1, &n2);
+
+    struct Node* lca = LCA(root, n1, n2);
+
+    if(lca)
+        printf("%d", lca->data);
+
+    return 0;
+}
