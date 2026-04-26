3f83854de4d0f03ad51271e81d4c96efecd0c0c7
ganneev <ganeevkaur2007@gmail.com>
2026-04-26 23:47:46 +0530

    Add files via upload

---
+++ question79.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <limits.h>
+
+int minDist(int dist[], int visited[], int n) {
+    int min = INT_MAX, idx = -1;
+    for (int i = 1; i <= n; i++) {
+        if (!visited[i] && dist[i] < min) {
+            min = dist[i];
+            idx = i;
+        }
+    }
+    return idx;
+}
+
+int main() {
+    int n, m;
+    scanf("%d %d", &n, &m);
+
+    int graph[n+1][n+1];
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            graph[i][j] = 0;
+        }
+    }
+
+    for (int i = 0; i < m; i++) {
+        int u, v, w;
+        scanf("%d %d %d", &u, &v, &w);
+        graph[u][v] = w;
+        graph[v][u] = w;
+    }
+
+    int source;
+    scanf("%d", &source);
+
+    int dist[n+1];
+    int visited[n+1];
+
+    for (int i = 1; i <= n; i++) {
+        dist[i] = INT_MAX;
+        visited[i] = 0;
+    }
+
+    dist[source] = 0;
+
+    for (int count = 1; count <= n; count++) {
+        int u = minDist(dist, visited, n);
+        visited[u] = 1;
+
+        for (int v = 1; v <= n; v++) {
+            if (graph[u][v] && !visited[v] && dist[u] + graph[u][v] < dist[v]) {
+                dist[v] = dist[u] + graph[u][v];
+            }
+        }
+    }
+
+    for (int i = 1; i <= n; i++) {
+        printf("%d ", dist[i]);
+    }
+
+    return 0;
+}
